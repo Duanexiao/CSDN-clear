e6c8a4d78c7d3c6f597e89228ab6e09b5afca27b
shiyajian <shiyajian1991@gmail.com>
2019-08-02 17:33:19 +0800

    update div selector

--- main.js
+++ main.js
@@ -3,3 +3,7 @@ document.getElementsByClassName("hide-article-box")[0].remove();
 
 // 调整正文高度，取消overhidden
 document.getElementById('article_content').removeAttribute('style');
+
+document.getElementsByClassName("login-mark")[0].remove();
+document.getElementById('passportbox').remove();
+
